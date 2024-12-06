a9e9396ff4231e3343894dfcacd2f01eeba12efa
Daniel Spiers <danjspiers@gmail.com>
2024-12-06 15:31:24 +0000

    created base page code for POP and litter pages

---
+++ src/pages/POPpage.cpp
@@ -0,0 +1,20 @@
+#include <QtWidgets>
+#include "POPpage.hpp"
+#include "dataset.hpp"
+
+POPPage::POPPage()
+{
+}
+
+void POPPage::createWidgets()
+{
+}
+
+void POPPage::arrangeWidgets()
+{
+}
+
+void POPPage::updateDataset(SampleDataset *data)
+{
+    dataset = data;
+}
---
+++ src/pages/litterpage.cpp
@@ -0,0 +1,20 @@
+#include <QtWidgets>
+#include "litterpage.hpp"
+#include "dataset.hpp"
+
+LitterPage::LitterPage()
+{
+}
+
+void LitterPage::createWidgets()
+{
+}
+
+void LitterPage::arrangeWidgets()
+{
+}
+
+void LitterPage::updateDataset(SampleDataset *data)
+{
+    dataset = data;
+}
